5d23f1c5b67ef2d4030aac9a38fcc94c2e4897b2
Erik Prenzlin <eprenzlin@gmail.com>
2020-03-15 13:39:35 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,14 @@
-# Your code goes here!
+l# Your code goes here!
+class Dog
+  def bark=(sound)
+    @this_is_bark = sound
+  end
+
+  def bark
+    puts @this_is_bark
+  end
+end
+
+snoopy = Dog.net
+snoopy.bark
+snoopy.bark 
